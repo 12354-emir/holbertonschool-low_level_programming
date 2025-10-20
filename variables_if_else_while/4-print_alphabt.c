a6dac4865200d9439abcd17453637a70f21a80f5
12354-emir <12354@holbertonstudents.com>
2025-10-20 13:13:49 +0000

    variables task 4 projec 2

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase except q and e
+ * main - Prints the alphabet in lowercase, except q and e
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        if (c != 'e' && c != 'q')
-            putchar(c);
-    }
-    putchar('\n');
-
-    return (0);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
