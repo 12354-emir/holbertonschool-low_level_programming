c9b2f30539b2ea114400360f72ca3dfd3962a6b5
12354-emir <12354@holbertonstudents.com>
2025-10-23 07:36:09 +0000

    tache 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - additonne  deux entier et le retourne
+* @a: premier entier
+* @b: deuxieme entier
+*
+* Return: la somme de a et b
+*/
+int add(int a, int b)
+{
+    return (a + b);
+}
