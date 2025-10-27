6a8f166610b83cd55e82c437e23316dea6c9e24d
12354-emir <12354@holbertonstudents.com>
2025-10-27 06:10:58 +0000

    Add 2-mul function following Betty style

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplie deux entiers
+ * @a: premier entier
+ * @b: second entier
+ *
+ * Return: résultat de la multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
