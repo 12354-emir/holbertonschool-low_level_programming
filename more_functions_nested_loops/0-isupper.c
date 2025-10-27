eb8894eda112b13a6ca2e8dda500421a3b5b0377
12354-emir <12354@holbertonstudents.com>
2025-10-27 06:02:16 +0000

    Add 0-isupper function following Betty style

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,13 @@
 /**
  * _isupper - Vérifie si le caractère est une majuscule
  * @c: caractère à tester
+ *
  * Return: 1 si majuscule, 0 sinon
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    else
-        return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
