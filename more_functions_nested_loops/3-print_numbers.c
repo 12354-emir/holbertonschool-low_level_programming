add8673c85cdfeb9527011fd1cebad3a39b91f8f
12354-emir <12354@holbertonstudents.com>
2025-10-27 06:14:00 +0000

    Add 3-print_numbers function following Betty style

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - Affiche les chiffres de 0 à 9 suivis d'un retour à la ligne
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0'); /* Convertit i en caractère ASCII */
+
+	_putchar('\n'); /* Retour à la ligne à la fin */
+}
