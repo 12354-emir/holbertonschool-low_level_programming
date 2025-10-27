88da0ce764cc2bdad7992ef2097804712cc3f93c
12354-emir <12354@holbertonstudents.com>
2025-10-27 05:58:40 +0000

    Add function _isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Vérifie si le caractère est une majuscule
+ * @c: caractère à tester
+ * Return: 1 si majuscule, 0 sinon
+ */
+int _isupper(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return (1);
+    else
+        return (0);
+}
