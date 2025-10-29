8cd8bedc8f981a8602a953809d5ed352b058fedf
12354-emir <12354@holbertonstudents.com>
2025-10-29 13:53:01 +0000

    add

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * puts_half - affiche la seconde moitie d'une chaine
- * @str: la chaine a afficher
+ * puts_half - affiche la seconde moitieÃd'une chaine
+ * @str: chaine afficher
  */
 void puts_half(char *str)
 {
-    int len = 0;
-    int start;
+	int len = 0;
+	int start;
 
-    while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+		len++;
 
-    if (len % 2 == 0)
-        start = len / 2;
-    else
-        start = (len + 1) / 2;
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len + 1) / 2;
 
-    while (str[start] != '\0')
-    {
-        _putchar(str[start]);
-        start++;
-    }
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
