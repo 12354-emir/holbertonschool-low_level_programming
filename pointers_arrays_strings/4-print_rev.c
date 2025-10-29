657b81458a8b329fe854be2c2eb6995e569a630e
12354-emir <12354@holbertonstudents.com>
2025-10-29 09:15:23 +0000

    add

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - affiche une chaine de caracteres en sens inverse
+ * @s: chaine a afficher
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
