ba197d26bab8efd1525ba01db1214a925cdc8177
12354-emir <12354@holbertonstudents.com>
2025-10-30 14:30:01 +0000

    add

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * string_toupper - convertit toutes les lettres minuscules d'une chaine en majuscules
- * @s: pointeur vers la chaine a modifier
- * Return: pointeur vers la chaine modifie
+ * string_toupper - Convertit toutes les lettres minuscules d'une chaine en majuscules
+ * @s: La chaine a modifier
+ *
+ * Return: Pointeur vers la chaine modifie
  */
 char *string_toupper(char *s)
 {
-	int i = 0;
+    int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] - 32;
-		i++;
-	}
+    while (s[i] != '\0')
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+            s[i] = s[i] - 32;
+        i++;
+    }
 
-	return (s);
+    return (s);
 }
