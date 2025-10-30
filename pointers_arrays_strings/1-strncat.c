6534016b01fd0b9b1352d4b0d40b7fef30d3cbac
12354-emir <12354@holbertonstudents.com>
2025-10-30 14:02:26 +0000

    add

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatène deux chaines avec une limite de n caracteres
+ * @dest: chaine de destination
+ * @src: chaine source
+ * @n: nombre maximum de caracteres a ajouter
+ *
+ * Return: pointeur vers la chaine resultante dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    while (dest[i] != '\0')
-        i++;
+	while (dest[i] != '\0')
+		i++;
 
-    while (j < n && src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    dest[i] = '\0';
-    return dest;
+	dest[i] = '\0';
+
+	return (dest);
 }
+
